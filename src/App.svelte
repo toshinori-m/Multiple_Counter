2e27ce23dea79cdc5c7146d793bb1c0cb75d69f5
toshinori-m <toshi3587@gmail.com>
2024-02-05 17:29:54 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+
+<script lang="ts">
+  export let name: string;
+
+  let counter = 0;
+
+  const increaseByOne = () => {
+    counter++;
+  };
+</script>
+
+<main>
+  <div>{name}</div>
+  <div>Counter: {counter}</div>
+  <button on:click={increaseByOne}>Add 1</button>
+</main>
