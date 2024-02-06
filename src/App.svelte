19712d7497816497a11adac0c0732bae019086e9
toshinori-m <toshi3587@gmail.com>
2024-02-07 08:40:57 +0900

    ui作成

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,57 @@
+<main>
+  <h1>Multiple Counter</h1>
+  <Counter />
+	<div class="container">
+		<div class="new-counter">
+			new counter
+		</div>
+	</div>
+  <div class="title-list">
+    title list: {titles.join(', ')}
+  </div>
+  <div class="sum-count">
+    sum of count: {count}
+  </div>
+</main>
 
-<script lang="ts">
-  export let name: string;
-
-  let counter = 0;
-
-  const increaseByOne = () => {
-    counter++;
-  };
+<script>
+  import Counter from './components/Counter.svelte';
+	let count = 0;
+  let titles = ['new'];
 </script>
 
-<main>
-  <div>{name}</div>
-  <div>Counter: {counter}</div>
-  <button on:click={increaseByOne}>Add 1</button>
-</main>
+<style>
+  main {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 4rem;
+		font-weight: inherit;
+    margin: 0;
+  }
+	.container {
+		display: flex;
+		justify-content: center;
+	}
+	.new-counter {
+		cursor: pointer;
+    font-size: 1rem;
+    text-align: center;
+		height: 24px;
+		margin: 0 auto;
+    color: white;
+		width: 384px;
+    background-color: #44c774d6;
+    border-radius: 3px;
+    margin: 0;
+  }
+
+	.title-list, .sum-count {
+    font-size: 1rem;
+    margin: 0;
+  }
+</style>
